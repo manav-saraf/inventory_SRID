4506b1d03667d9fcd3ffd53192a3f7ea8da221d3
manav-saraf <manav_s@ec.iitr.ac.in>
2022-07-06 11:56:48 +0530

    Database setup

--- portal/models.py
+++ portal/models.py
@@ -1,3 +1,25 @@
+from pyexpat import model
 from django.db import models
 
 # Create your models here.
+
+
+class Employee(models.Model):
+    name = models.CharField(max_length=50)
+    emp_id = models.CharField(max_length=30)
+    samples = models.ManyToManyField('Sample', through='Ownership', related_name='employees')
+
+
+class Sample(models.Model):
+    sample_id = models.CharField(max_length=30)
+    model_code = models.CharField(max_length=30)
+    subcategory = models.CharField(max_length=30)
+    working_condition = models.BooleanField()
+
+
+class Ownership(models.Model):
+    owner = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    sample = models.ForeignKey(Sample, on_delete=models.CASCADE)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    ownership_type = models.TextChoices('OwnershipType', 'PERMANENT TEMPORARY')
